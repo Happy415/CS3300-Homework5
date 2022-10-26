92bda67860ae8ac3942ff02771a7502dcd1752fa
Alex <alexandertraffis@gmail.com>
2022-10-26 03:07:35 +0000

    Current code

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -7,6 +7,7 @@ def sum arr
 
   result = 0
 
+  # Sums up every number in the array
   for num in arr do
 
   	result += num
@@ -20,32 +21,41 @@ end
 def max_2_sum arr
   # YOUR CODE HERE
 
+  # Initialize variables
   max1 = 0
   max2 = 0
   len = arr.length
   result = 0
 
+  # If the length of the array is greater than 1
   if len > 1
     
+    # Sort the array and get the two largest numbers which are at the end
+    # of the array now
     newArr = arr.sort
     max1 = newArr[len - 1]
     max2 = newArr[len - 2]
     result = max1+max2
 
+  # If the array only contains one element
   elsif len == 1
     result = arr[0]
   end
 
-  result
+  # If array is empty result will remain 0
+  return result
 
 end 
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
-  
-  if arr.length > 1
-    arr.combination(2).any? {|num1, num2| num1 + num2 == n}
 
+  # If the length of the array is greater than 1
+  if arr.length > 1
+    # Any combination of number will be tested to see if they add up to n
+    return arr.combination(2).any? {|num1, num2| num1 + num2 == n}
+
+  # If none add up to n it will return false
   else
     return false
   end
@@ -55,13 +65,20 @@ end
 
 def hello(name)
   # YOUR CODE HERE
-  "Hello, #{name}"
+
+  # Returns new string with the name attached
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
 
+  # Sets characters that need to be excluded from the search
   consonant = /^[^aeiouAEIOU\d\W_]/i
+
+  # Because consonant will return false if any vowels are
+  # used to start, it will return true if any consonants are 
+  # used to start
   if consonant.match(s)
     return true
   else 
@@ -72,8 +89,12 @@ end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
-  multiple = /^[01]+$/.match(s)
-  if  multiple && s.to_i(2) % 4 == 0
+
+  # Sets regex to match binary numbers in the string
+  binary = /^[01]+$/.match(s)
+
+  # Checks if the binary number in the string in divisible by 4
+  if  binary && s.to_i(2) % 4 == 0
     return true
   
   else
@@ -85,6 +106,8 @@ end
 # Part 3
 
 class BookInStock
+
+  # Constructor of the class
   def initialize(isbn, price)
     @isbn = isbn
     @price = price
